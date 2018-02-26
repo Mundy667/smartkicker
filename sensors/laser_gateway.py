6afd73fdcf542f5c94cb213bd0e764d831058248
Mundy667 <svenmundschenk@gmail.com>
2018-02-26 11:28:03 +0100

    add sql connection to laser_gateway.py

--- sensors/laser_gateway.py
+++ sensors/laser_gateway.py
@@ -1,13 +1,46 @@
 import RPi.GPIO as GPIO
 import os, time
+import MySQLdb as mdb
+
 
 RECEIVER_A_PIN = 23
+RECEIVER_B_PIN = 24
 
 def callback_func(channel):
     if GPIO.input(channel):
-        print("Lichtschranke unterbrochen")
-        # hier kommt dann der http post zum core hin
+        #print("Lichtschranke unterbrochen: ", channel)
         # darauf achten dass hier auch das richtige team ausgewertet wird
+        host = '192.168.111.12'
+        user = 'root'
+        password = 'kicker'
+        port = 8989
+        db = 'kicker'
+
+        con = mdb.connect(
+        host=host,
+        user=user,
+        passwd=password,
+        port=port,
+        db=db
+        )
+
+        cur = con.cursor()
+        #Retriev current gameId
+        cur.execute("SELECT * FROM games ORDER BY id DESC LIMIT 0,1")
+
+        row = cur.fetchone()
+
+        currentGameId = int(row[1])
+
+        if channel == RECEIVER_A_PIN:
+            teamGoal = 'A'
+        else: 
+            teamGoal = 'B'
+        print("GOAAAL - Team ", teamGoal)
+        sql = "INSERT INTO games(gameId, team) VALUES(%d, '%s')" % (currentGameId, teamGoal)
+        cur.execute(sql)
+        con.commit()
+
 
 if __name__ == '__main__':
 
@@ -15,7 +48,10 @@ if __name__ == '__main__':
     GPIO.setwarnings(False)
     # bin B spaeter nicht vergessen
     GPIO.setup(RECEIVER_A_PIN, GPIO.IN)
+    GPIO.setup(RECEIVER_B_PIN, GPIO.IN)
     GPIO.add_event_detect(RECEIVER_A_PIN, GPIO.RISING, callback=callback_func, bouncetime=200)
+    GPIO.add_event_detect(RECEIVER_B_PIN, GPIO.RISING, callback=callback_func, bouncetime=200)
+
 
     try:
         while True:
@@ -23,3 +59,6 @@ if __name__ == '__main__':
     except:
         # Event wieder entfernen mittels: 
         GPIO.remove_event_detect(RECEIVER_A_PIN)
+        GPIO.remove_event_detect(RECEIVER_B_PIN)
+
+       
